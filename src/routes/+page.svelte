f1bd7266519b49c53815091e095f86f107a3e4fe
Rebecca Chapman <chapman.reb@gmail.com>
2025-01-31 16:22:07 -0500

    tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1>Rebecca Chapman, PhD</h1>
+<p>Made with <a href="https://svelte.dev/docs/kit">SvelteKit</a></p>
